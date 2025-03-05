b5c64c9af37e7c47b7867fb02ad2aa16b2ea4a60
yvoste <yves.mitaine@nordnet.fr>
2025-03-05 19:04:31 +0100

    gfdgd

--- migrations/Version20250305175756.php
+++ migrations/Version20250305175756.php
@@ -20,6 +20,6 @@ final class Version20250305175756 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE fraise (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE lolo (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
     }
 }
