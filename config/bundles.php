ceabdac128f6b1e5af32ddd33ed1b01de6265d57
yvoste <yves.mitaine@nordnet.fr>
2025-03-06 19:04:36 +0100

    clean il

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
-    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
